8b658be1b63716afeb3d062b65ded08fc2aed894
iujunhyung <junehyung@iyulab.com>
2025-10-29 21:17:24 +0900

    update

--- dist/components/rich-text-editor/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { RichTextEditor } from './RichTextEditor';
-declare global {
-    interface HTMLElementTagNameMap {
-        "u-rich-text-editor": RichTextEditor;
-    }
-}
-declare const URichTextEditor: import('@lit/react').ReactWebComponent<RichTextEditor, {}>;
-export { RichTextEditor, URichTextEditor };
